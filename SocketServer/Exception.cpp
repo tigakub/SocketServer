8b42e82addc49fd4b9d2291d18ea4585eb14a2fd
tigakub <tigakub@mac.com>
2022-03-25 16:07:31 -0700

    Extraneous Directory!
    Argh git is such a pain

--- SocketServer/Exception.cpp
+++
@@ -1,7 +0,0 @@
-#include "Exception.hpp"
-
-ostream &operator<<(ostream &os, Exception &e) {
-    os << "Exception caught in file \"" << e.file << "\", on line " << e.line << ", with error " << e.err << ": " << e.msg;
-    return os;
-}
-
